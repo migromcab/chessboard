756a8d825558886e602f2dce47c8ed51ddfa51e9
migromcab <migromcab@icloud.com>
2024-02-27 15:30:22 +0100

    created chess square

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div id='Chessboard'>
+
+      <div className='tiles'>
+
+      </div>
+
+    </div>
+  )
+  
+    
+}
+
+export default App
